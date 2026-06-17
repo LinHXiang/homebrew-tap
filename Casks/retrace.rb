e1eede2c08d2385e1a6df2a4dc7ddfe03a4d5a44
lin <linhaoxiang@shareintelli.com>
2026-06-17 15:48:58 +0800

    Update Retrace cask to rc2

--- Casks/retrace.rb
+++ Casks/retrace.rb
@@ -1,6 +1,6 @@
 cask "retrace" do
-  version "1.0.0-rc1"
-  sha256 "1132a319c3c9a93d9a63ce37c81beede2976853932f8d615fbc259733262e434"
+  version "1.0.0-rc2"
+  sha256 "f0bb1a851fc822b90f9443a2e583413f123939849e202f05ef576158a36a230b"
 
   url "https://github.com/LinHXiang/Retrace/releases/download/v#{version}/Retrace-#{version}.zip"
   name "Retrace"
