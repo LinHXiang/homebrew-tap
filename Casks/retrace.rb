1c70e5aaa98249f0e7510598a84c03ff218a2576
L.L <dj7946@qq.com>
2026-06-17 22:54:30 +0800

    Update Retrace cask to 1.0.1

--- Casks/retrace.rb
+++ Casks/retrace.rb
@@ -1,6 +1,6 @@
 cask "retrace" do
-  version "1.0.0"
-  sha256 "fe2aa22c328096c17e6ea606aecfba0c95c68a300fc60c7b78c66452dc50e4a8"
+  version "1.0.1"
+  sha256 "a3aa54d4f7a74530ae1843440a9d2ceea4325ec635411921e968163c840ea08b"
 
   url "https://github.com/LinHXiang/Retrace/releases/download/v#{version}/Retrace-#{version}.zip"
   name "Retrace"
